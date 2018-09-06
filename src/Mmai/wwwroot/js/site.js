b67491c3c2b61159e7a9d55998f37e1faf6b674a
Jakub Linhart <jakub.linhart@gmail.com>
2018-09-06 11:52:24 +0200

    Initial import.

---
+++ src/Mmai/wwwroot/js/site.js
@@ -0,0 +1,218 @@
+﻿var audioSet = [
+    {
+        subSets: [
+            [
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_10.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_11.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_12.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_14.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_15.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_16.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_20.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_22.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_25.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_27.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_31.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_33.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_38.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_40.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_42.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_44.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_47.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_49.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_51.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_52.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_53.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_54.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_58.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_6.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_64.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_66.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_68.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_69.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_70.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_72.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_73.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_74.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_8.wav",
+                "sounds/001/108_ci_109_2013/108_ci_109_2013_9.wav"
+            ],
+            [
+                "sounds/001/108-109b_2014/108-109b_2014_10.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_11.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_12.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_13.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_14.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_15.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_16.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_17.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_18.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_19.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_2.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_20.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_21.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_3.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_4.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_5.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_6.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_7.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_8.wav",
+                "sounds/001/108-109b_2014/108-109b_2014_9.wav",
+            ]
+        ]
+    },
+
+    {
+        subSets: [
+            [
+                "sounds/002/193_2013/193_2013_10.wav",
+                "sounds/002/193_2013/193_2013_11.wav",
+                "sounds/002/193_2013/193_2013_12.wav",
+                "sounds/002/193_2013/193_2013_13.wav",
+                "sounds/002/193_2013/193_2013_14.wav",
+                "sounds/002/193_2013/193_2013_15.wav",
+                "sounds/002/193_2013/193_2013_16.wav",
+                "sounds/002/193_2013/193_2013_17.wav",
+                "sounds/002/193_2013/193_2013_18.wav",
+                "sounds/002/193_2013/193_2013_19.wav",
+                "sounds/002/193_2013/193_2013_2.wav",
+                "sounds/002/193_2013/193_2013_20.wav",
+                "sounds/002/193_2013/193_2013_21.wav",
+                "sounds/002/193_2013/193_2013_22.wav",
+                "sounds/002/193_2013/193_2013_23.wav",
+                "sounds/002/193_2013/193_2013_24.wav",
+                "sounds/002/193_2013/193_2013_25.wav",
+                "sounds/002/193_2013/193_2013_3.wav",
+                "sounds/002/193_2013/193_2013_5.wav",
+                "sounds/002/193_2013/193_2013_6.wav",
+                "sounds/002/193_2013/193_2013_7.wav",
+                "sounds/002/193_2013/193_2013_8.wav",
+                "sounds/002/193_2013/193_2013_9.wav",
+            ],
+            [
+                "sounds/002/193b_2014/193b_2014_1.wav",
+                "sounds/002/193b_2014/193b_2014_10.wav",
+                "sounds/002/193b_2014/193b_2014_11.wav",
+                "sounds/002/193b_2014/193b_2014_12.wav",
+                "sounds/002/193b_2014/193b_2014_13.wav",
+                "sounds/002/193b_2014/193b_2014_14.wav",
+                "sounds/002/193b_2014/193b_2014_15.wav",
+                "sounds/002/193b_2014/193b_2014_16.wav",
+                "sounds/002/193b_2014/193b_2014_17.wav",
+                "sounds/002/193b_2014/193b_2014_18.wav",
+                "sounds/002/193b_2014/193b_2014_19.wav",
+                "sounds/002/193b_2014/193b_2014_2.wav",
+                "sounds/002/193b_2014/193b_2014_20.wav",
+                "sounds/002/193b_2014/193b_2014_21.wav",
+                "sounds/002/193b_2014/193b_2014_22.wav",
+                "sounds/002/193b_2014/193b_2014_23.wav",
+                "sounds/002/193b_2014/193b_2014_24.wav",
+                "sounds/002/193b_2014/193b_2014_25.wav",
+                "sounds/002/193b_2014/193b_2014_26.wav",
+                "sounds/002/193b_2014/193b_2014_27.wav",
+                "sounds/002/193b_2014/193b_2014_3.wav",
+                "sounds/002/193b_2014/193b_2014_4.wav",
+                "sounds/002/193b_2014/193b_2014_5.wav",
+                "sounds/002/193b_2014/193b_2014_6.wav",
+                "sounds/002/193b_2014/193b_2014_7.wav",
+                "sounds/002/193b_2014/193b_2014_8.wav",
+                "sounds/002/193b_2014/193b_2014_9.wav",
+            ]
+        ]
+    }
+]
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+(function () {
+    var soundPlaying = false;
+    var width = 2;
+    var height = 2;
+    var cardCount = width * height;
+    var setsCount = audioSet.length;
+    var firstSelectedCard = null;
+
+    if (cardCount < setsCount * 2) {
+        // TODO:
+        alert('More cards than sets.')
+    }
+
+    var cards = [];
+    for (var i = 0; i < setsCount; i++) {
+        var randomizedSubSets = [];
+        for (var j = 0; j < audioSet[i].subSets.length; j++) {
+            var subSetUrlIndex = getRandomInt(0, audioSet[i].subSets[j].length - 1);
+            randomizedSubSets.push({ weight: getRandomInt(0, 1000), url: audioSet[i].subSets[j][subSetUrlIndex] });
+        }
+
+        randomizedSubSets.sort(function (x, y) {
+            return x.weight - y.weight;
+        });
+
+
+        cards.push({
+            setIndex: i,
+            weight: getRandomInt(0, 1000),
+            url: randomizedSubSets[0].url
+        });
+        cards.push({
+            setIndex: i,
+            weight: getRandomInt(0, 1000),
+            url: randomizedSubSets[1].url
+        });
+    }
+    cards.sort(function (x, y) {
+        return x.weight - y.weight;
+    });
+
+    for (var i = 0; i < cardCount; i++) {
+        var card = {
+            cardId: "#card" + i,
+            url: cards[i].url,
+            setIndex: cards[i].setIndex
+        }
+        console.log(card);
+
+        $(document).on("click", card.cardId, card, function (e) {
+            if (soundPlaying)
+                return;
+
+            soundPlaying = true;
+            var cardId = e.data.cardId;
+            var url = e.data.url;
+            var card = e.data;
+
+            $(cardId).addClass("cardPlaying");
+            $(cardId).removeClass("cardCovered");
+            $(cardId).addClass("cardUncovered");
+            var audio = new Audio(url);
+            audio.onended = function () {
+                $(card.cardId).removeClass("cardPlaying");
+                soundPlaying = false;
+
+                console.log("Sound finished for card: ");
+                console.log(card);
+                console.log("First selected card: " + firstSelectedCard);
+                console.log(firstSelectedCard);
+
+                if (firstSelectedCard != null && firstSelectedCard.cardId != card.cardId) {
+                    if (firstSelectedCard.setIndex != card.setIndex) {
+                        $(card.cardId).removeClass("cardUncovered");
+                        $(card.cardId).addClass("cardCovered");
+                        $(firstSelectedCard.cardId).removeClass("cardUncovered");
+                        $(firstSelectedCard.cardId).addClass("cardCovered");
+                    }
+                    firstSelectedCard = null;
+                }
+                else {
+                    firstSelectedCard = card;
+                }
+            };
+            audio.play();
+            console.log("Card selected: ");
+            console.log(card)
+        });
+    }
+})();
