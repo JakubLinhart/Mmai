bed1dcc22d404427c16aa69846921e1a263f73ea
Jakub Linhart <jakub.linhart@gmail.com>
2018-09-11 12:38:55 +0200

    Adding more sets.

--- src/Mmai/Controllers/HomeController.cs
+++ src/Mmai/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Mmai.Data;
 using Mmai.Models;
 
 namespace Mmai.Controllers
@@ -12,6 +14,15 @@ namespace Mmai.Controllers
     {
         public IActionResult Index()
         {
+            var playerId = Request.Cookies["playerId"];
+
+            if (string.IsNullOrEmpty(playerId))
+            {
+                var options = new CookieOptions();
+                options.Expires = DateTime.Now.AddDays(14);
+                Response.Cookies.Append("playerId", Repository.NewGuid(), options);
+            }
+
             return View();
         }
 
--- src/Mmai/wwwroot/js/site.js
+++ src/Mmai/wwwroot/js/site.js
@@ -1,5 +1,6 @@
-﻿var audioSet = [
+﻿var species = [
     {
+        name: "Jiřička Hrůzostrašná",
         subSets: [
             [
                 "sounds/001/108_ci_109_2013/108_ci_109_2013_10.wav",
@@ -61,8 +62,8 @@
             ]
         ]
     },
-
     {
+        name: "Satan Klausův",
         subSets: [
             [
                 "sounds/002/193_2013/193_2013_10.wav",
@@ -119,6 +120,122 @@
                 "sounds/002/193b_2014/193b_2014_9.wav",
             ]
         ]
+    },
+    {
+        name: "Krůta Ukrutná",
+        subSets: [
+            [
+                "sounds/003/229_spont_2013/229_spont_2013_10.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_11.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_12.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_13.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_14.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_15.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_16.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_17.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_18.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_19.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_2.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_3.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_4.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_5.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_6.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_7.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_8.wav",
+                "sounds/003/229_spont_2013/229_spont_2013_9.wav"
+            ],
+            [
+                "sounds/003/229b_2014/229b_2014_10.wav",
+                "sounds/003/229b_2014/229b_2014_11.wav",
+                "sounds/003/229b_2014/229b_2014_12.wav",
+                "sounds/003/229b_2014/229b_2014_13.wav",
+                "sounds/003/229b_2014/229b_2014_14.wav",
+                "sounds/003/229b_2014/229b_2014_15.wav",
+                "sounds/003/229b_2014/229b_2014_16.wav",
+                "sounds/003/229b_2014/229b_2014_17.wav",
+                "sounds/003/229b_2014/229b_2014_18.wav",
+                "sounds/003/229b_2014/229b_2014_19.wav",
+                "sounds/003/229b_2014/229b_2014_2.wav",
+                "sounds/003/229b_2014/229b_2014_20.wav",
+                "sounds/003/229b_2014/229b_2014_21.wav",
+                "sounds/003/229b_2014/229b_2014_22.wav",
+                "sounds/003/229b_2014/229b_2014_23.wav",
+                "sounds/003/229b_2014/229b_2014_24.wav",
+                "sounds/003/229b_2014/229b_2014_25.wav",
+                "sounds/003/229b_2014/229b_2014_3.wav",
+                "sounds/003/229b_2014/229b_2014_4.wav",
+                "sounds/003/229b_2014/229b_2014_5.wav",
+                "sounds/003/229b_2014/229b_2014_6.wav",
+                "sounds/003/229b_2014/229b_2014_7.wav",
+                "sounds/003/229b_2014/229b_2014_8.wav",
+                "sounds/003/229b_2014/229b_2014_9.wav",
+            ]
+        ]
+    },
+    {
+        name: "Pěnkava Umrlá",
+        subSets: [
+            [
+                "sounds/004/7_2013/7_2013_1.wav",
+                "sounds/004/7_2013/7_2013_12.wav",
+                "sounds/004/7_2013/7_2013_13.wav",
+                "sounds/004/7_2013/7_2013_14.wav",
+                "sounds/004/7_2013/7_2013_15.wav",
+                "sounds/004/7_2013/7_2013_16.wav",
+                "sounds/004/7_2013/7_2013_17.wav",
+                "sounds/004/7_2013/7_2013_2.wav",
+                "sounds/004/7_2013/7_2013_4.wav",
+                "sounds/004/7_2013/7_2013_8.wav",
+            ],
+            [
+                "sounds/004/7_2014/7_2014_10.wav",
+                "sounds/004/7_2014/7_2014_11.wav",
+                "sounds/004/7_2014/7_2014_12.wav",
+                "sounds/004/7_2014/7_2014_13.wav",
+                "sounds/004/7_2014/7_2014_14.wav",
+                "sounds/004/7_2014/7_2014_15.wav",
+                "sounds/004/7_2014/7_2014_16.wav",
+                "sounds/004/7_2014/7_2014_17.wav",
+                "sounds/004/7_2014/7_2014_2.wav",
+                "sounds/004/7_2014/7_2014_3.wav",
+                "sounds/004/7_2014/7_2014_4.wav",
+                "sounds/004/7_2014/7_2014_5.wav",
+                "sounds/004/7_2014/7_2014_6.wav",
+                "sounds/004/7_2014/7_2014_7.wav",
+                "sounds/004/7_2014/7_2014_8.wav",
+                "sounds/004/7_2014/7_2014_9.wav"
+            ]
+        ]
+    },
+    {
+        name: "Ostuda Obecná",
+        subSets: [
+            [
+                "sounds/005/94_2013/94_2013_10.wav",
+                "sounds/005/94_2013/94_2013_2.wav",
+                "sounds/005/94_2013/94_2013_3.wav",
+                "sounds/005/94_2013/94_2013_4.wav",
+                "sounds/005/94_2013/94_2013_5.wav",
+            ],
+            [
+                "sounds/005/94_2014/94_2014_10.wav",
+                "sounds/005/94_2014/94_2014_11.wav",
+                "sounds/005/94_2014/94_2014_12.wav",
+                "sounds/005/94_2014/94_2014_13.wav",
+                "sounds/005/94_2014/94_2014_14.wav",
+                "sounds/005/94_2014/94_2014_15.wav",
+                "sounds/005/94_2014/94_2014_16.wav",
+                "sounds/005/94_2014/94_2014_17.wav",
+                "sounds/005/94_2014/94_2014_2.wav",
+                "sounds/005/94_2014/94_2014_3.wav",
+                "sounds/005/94_2014/94_2014_4.wav",
+                "sounds/005/94_2014/94_2014_5.wav",
+                "sounds/005/94_2014/94_2014_6.wav",
+                "sounds/005/94_2014/94_2014_7.wav",
+                "sounds/005/94_2014/94_2014_8.wav",
+                "sounds/005/94_2014/94_2014_9.wav"
+            ]
+        ]
     }
 ]
 
@@ -128,51 +245,101 @@ function getRandomInt(min, max) {
 
 (function () {
     var soundPlaying = false;
-    var width = 2;
-    var height = 2;
-    var cardCount = width * height;
-    var setsCount = audioSet.length;
+    var cardCount = 10;
+    var setsCount = species.length;
     var firstSelectedCard = null;
+    var matchCount = 0;
+    var lastEventTime = null;
+    var gameId = null;
+    var won = false;
 
-    if (cardCount < setsCount * 2) {
+    function postGameEvent(label, card) {
+        var now = new Date();
+        var sinceLastEvent;
+
+        if (lastEventTime != null) {
+            sinceLastEvent = now - lastEventTime;
+        }
+        else
+            sinceLastEvent = null;
+
+        var data = JSON.stringify({
+            label: label,
+            card: card,
+            time: now,
+            millisecondsSinceLastEvent: sinceLastEvent,
+            gameId: gameId
+        });
+        lastEventTime = now;
+        $.ajax({
+            url: "/api/events/",
+            type: "post",
+            data: data,
+            dataType: "json",
+            accept: 'application/json',
+            contentType: "application/json",
+
+            success: function (data) {
+                gameId = data;
+            },
+            error: function (result) {
+                alert(result);
+            }
+        });
+    }
+
+    if (cardCount > setsCount * 2) {
         // TODO:
         alert('More cards than sets.')
     }
 
     var cards = [];
+    var randomizedSpecies = [];
     for (var i = 0; i < setsCount; i++) {
+        randomizedSpecies.push({ index: i, weight: getRandomInt(0, 1000), species: species[i] });
+    }
+    randomizedSpecies.sort(function (x, y) {
+        return x.weight - y.weight;
+    });
+
+    speciesIndex = 0;
+    for (var i = 0; i < cardCount; i += 2, speciesIndex++) {
         var randomizedSubSets = [];
-        for (var j = 0; j < audioSet[i].subSets.length; j++) {
-            var subSetUrlIndex = getRandomInt(0, audioSet[i].subSets[j].length - 1);
-            randomizedSubSets.push({ weight: getRandomInt(0, 1000), url: audioSet[i].subSets[j][subSetUrlIndex] });
+        var subSets = randomizedSpecies[speciesIndex].species.subSets;
+
+        for (var j = 0; j < subSets.length; j++) {
+            var subSetUrlIndex = getRandomInt(0, subSets[j].length - 1);
+            randomizedSubSets.push({ weight: getRandomInt(0, 1000), url: randomizedSpecies[speciesIndex].species.subSets[j][subSetUrlIndex] });
         }
 
         randomizedSubSets.sort(function (x, y) {
             return x.weight - y.weight;
         });
 
-
         cards.push({
-            setIndex: i,
+            setIndex: randomizedSpecies[speciesIndex].index,
             weight: getRandomInt(0, 1000),
-            url: randomizedSubSets[0].url
+            url: randomizedSubSets[0].url,
+            name: randomizedSpecies[speciesIndex].species.name
         });
         cards.push({
-            setIndex: i,
+            setIndex: randomizedSpecies[speciesIndex].index,
             weight: getRandomInt(0, 1000),
-            url: randomizedSubSets[1].url
+            url: randomizedSubSets[1].url,
+            name: randomizedSpecies[speciesIndex].species.name
         });
     }
+
     cards.sort(function (x, y) {
         return x.weight - y.weight;
     });
 
+    postGameEvent("started", null);
     for (var i = 0; i < cardCount; i++) {
-        var card = {
-            cardId: "#card" + i,
-            url: cards[i].url,
-            setIndex: cards[i].setIndex
-        }
+        cards[i].cardId = "#card" + i;
+        var card = cards[i];
+        $(card.cardId).addClass("cardCovered");
+        $(card.cardId).empty();
         console.log(card);
 
         $(document).on("click", card.cardId, card, function (e) {
@@ -197,19 +364,38 @@ function getRandomInt(min, max) {
                 console.log("First selected card: " + firstSelectedCard);
                 console.log(firstSelectedCard);
 
-                if (firstSelectedCard != null && firstSelectedCard.cardId != card.cardId) {
-                    if (firstSelectedCard.setIndex != card.setIndex) {
-                        $(card.cardId).removeClass("cardUncovered");
-                        $(card.cardId).addClass("cardCovered");
-                        $(firstSelectedCard.cardId).removeClass("cardUncovered");
-                        $(firstSelectedCard.cardId).addClass("cardCovered");
+                if (matchCount < cardCount) {
+                    if (firstSelectedCard != null && firstSelectedCard.cardId != card.cardId) {
+                        if (firstSelectedCard.setIndex != card.setIndex) {
+                            $(card.cardId).removeClass("cardUncovered");
+                            $(card.cardId).addClass("cardCovered");
+                            $(firstSelectedCard.cardId).removeClass("cardUncovered");
+                            $(firstSelectedCard.cardId).addClass("cardCovered");
+                            postGameEvent("mismatch", card.url);
+                        }
+                        else {
+                            matchCount += 2;
+                            console.log(matchCount + ", " + cardCount)
+                            if (matchCount >= cardCount) {
+                                for (var i = 0; i < cardCount; i++) {
+                                    console.log(cards[i]);
+                                    $(cards[i].cardId).text(cards[i].name);
+                                }
+                                won = true;
+                            }
+                            postGameEvent("match", card.url);
+                        }
+                        firstSelectedCard = null;
                     }
-                    firstSelectedCard = null;
+                    else {
+                        postGameEvent("first", card.url);
+                        firstSelectedCard = card;
+                    }
                 }
                 else {
-                    firstSelectedCard = card;
+                    postGameEvent("after the win", card.url);
                 }
-            };
+            }
             audio.play();
             console.log("Card selected: ");
             console.log(card)
