41fe7b30ba52f2d8e928f7c2652b6be6c736f824
Jakub Linhart <jakub.linhart@gmail.com>
2018-09-24 18:16:31 +0200

    Cleaning up texts.

--- src/Mmai/Models/SpeciesRepository.cs
+++ src/Mmai/Models/SpeciesRepository.cs
@@ -16,11 +16,11 @@ namespace Mmai.Models
             speciesStubs = new Dictionary<string, SpeciesStub>(StringComparer.OrdinalIgnoreCase)
             {
                 { "littleowl", new SpeciesStub("littleowl", @"\sounds\littleowl\", "Little Owl", 20, 5, CreateSpecies,
-                    "In the first game, you will hear the territorial calls of Little owl (Athene noctua).") },
+                    "In this game, you will hear the territorial calls of Little owl (Athene noctua).") },
                 { "10vars", new SpeciesStub("10vars", @"\sounds\10vars\", "10vars", 20, 5, CreateSpecies,
-                    "In subsequent games, you will hear synthetic acoustic signatures from the two non-existing species which we made up for purposes of this pilot experiment.") },
+                    "Now, you will hear synthetic acoustic signatures from the two non-existing species which we made up for purposes of this pilot experiment.") },
                 { "3vars", new SpeciesStub("3vars", @"\sounds\3vars\", "3vars", 20, 5, CreateSpecies,
-                    "In subsequent games, you will hear synthetic acoustic signatures from the two non-existing species which we made up for purposes of this pilot experiment. ")}
+                    "Now, you will hear synthetic acoustic signatures from the two non-existing species which we made up for purposes of this pilot experiment. ")}
             };
         }
 
